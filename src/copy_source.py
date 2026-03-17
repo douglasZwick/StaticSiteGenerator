0db3817ff52679c775308d939c06e3d3fa520967
Doug Zwick <modusponens@gmail.com>
2026-03-17 12:29:26 -0700

    Quick edits to improve readability and logic flow, and also renamed build to copy_source

--- src/copy_source.py
+++ src/copy_source.py
@@ -2,15 +2,7 @@ import os
 import shutil
 
 
-def copy_to_dst(src_path: str, dst_root: str, src_dir: str = "") -> None:
-  if os.path.exists(dst_root):
-    shutil.rmtree(dst_root)
-  os.mkdir(dst_root)
-
-  _copy_to_dst_r(src_path, dst_root, src_dir)
-
-
-def _copy_to_dst_r(src_path: str, dst_root: str, src_dir: str = "") -> None:
+def copy_source(src_path: str, dst_root: str, src_dir: str = "") -> None:
   with os.scandir(src_path) as entries:
     for entry in entries:
       if os.path.isfile(entry):
@@ -21,4 +13,4 @@ def _copy_to_dst_r(src_path: str, dst_root: str, src_dir: str = "") -> None:
         shutil.copy(entry.path, dst_path)
       else:
         next_src_dir = os.path.join(src_dir, entry.name)
-        _copy_to_dst_r(entry.path, dst_root, next_src_dir)
+        copy_source(entry.path, dst_root, next_src_dir)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
+import os
+import shutil
 from parentnode import *
 from leafnode import *
 from inline_markdown import *
 from block_markdown import *
-from build import copy_to_dst
+from copy_source import copy_source
+
+
+STATIC_DIR_PATH: str = "./static"
+PUBLIC_DIR_PATH: str = "./public"
 
 
 def main() -> None:
-  copy_to_dst("static", "public")
+  if os.path.exists(PUBLIC_DIR_PATH):
+    print("Cleaning up existing public directory...")
+    shutil.rmtree(PUBLIC_DIR_PATH)
+  
+  copy_source(STATIC_DIR_PATH, PUBLIC_DIR_PATH)
 
 
 main()
